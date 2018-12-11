6dc1c278441190fbfc88e38b49cd6f770f9470d1
Felix Wetell <felix.wetell@gmail.com>
2018-12-11 17:03:15 +0100

    And som other files

--- C/Labs/lottery/main.c
+++ C/Labs/lottery/main.c
@@ -3,22 +3,51 @@
 #include <limits.h>
 #include <string.h>
 #include <time.h>
+#include "prefixes.h"
 
-int * randomise ( int array[7] )
+int duplicate( int array[lotteryLength], int number )
 {
+    int answer = 0, i;
 
-    for ( int i = 0; i < 7; ++i )
+    for ( i = 0; i < lotteryLength; i++ )
     {
-        int r = rand() % 34 + 1;
-        array[i] = r;
+        if ( array[i] == number )
+        {
+            answer = 1;
+        }
     }
+    return answer;
+}
 
+int * randomise ( int array[lotteryLength] )
+{
+    int random;
+
+    for ( int i = 0; i < lotteryLength; i++ )
+    {
+        random = rand() % max + min;
+
+        while( duplicate( array, random ) == 1 )
+        {
+            random = rand() % max + min;
+        }
+        array[i] = random;
+    }
     return array;
 }
 
 int main()
 {
-    int input, lotteryRow[7];
+    srand(time(NULL));
+    int input, rowInput, lotteryRows[maxRows][lotteryLength], lotteryRow[lotteryLength], temp = 0, rowTemp;
+
+    char* filename = "/Users/felix/Documents/Github/university/C/Labs/lottery/lottery.txt";
+
+    FILE * fileWrite;
+    FILE * fileRead;
+
+    char * line = NULL;
+    size_t len = 0;
 
     do
     {
@@ -30,33 +59,93 @@ int main()
         printf("3. Read an existing lottery row from a file\n");
         printf("0. Exit the program \n");
         printf("Enter your menu choice: ");
-        scanf("%i", &input);
+        scanf( "%i", &input );
         printf("\n");
 
         switch ( input )
         {
             case 0:
                 printf( "Exiting program" );
-                printf( "\n" );
                 break;
 
             case 1:
-                randomise( lotteryRow );
-                for (int i = 0; i < (int)( sizeof(lotteryRow) / sizeof(lotteryRow[7]) ); ++i)
-                {
-                    printf( "%i ", lotteryRow[i] );
-                }
+
+                printf( "Enter number of rows: " );
+                scanf( "%i", &rowInput );
                 printf( "\n" );
+
+                for( int i = 0; i < rowInput; ++i )
+                {
+                    randomise( lotteryRow );
+
+                    for( int j = 0; j < lotteryLength; ++j )
+                    {
+                        lotteryRows[ i ][ j ] = lotteryRow[j];
+                        printf( "%i ", lotteryRows[i][j] );
+                    }
+                    printf( "\n" );
+                }
+
+                printf( "\n" );
+                printf(": : : : : : : : : : : : : : : : : : : \n");
+                printf( "Lottery rows successfully generated \n" );
+                printf(": : : : : : : : : : : : : : : : : : : \n");
+
+                temp = 1;
+                rowTemp = rowInput;
+
                 break;
 
             case 2:
+
+                if( temp >= 1 )
+                {
+                    fileWrite = fopen( filename, "w" );
+
+                    for( int i = 0; i < rowTemp; ++i )
+                    {
+                        for( int j = 0; j < lotteryLength; ++j )
+                        {
+                            fprintf( fileWrite, "%i ", lotteryRows[i][j] );
+                        }
+                        fprintf( fileWrite, "\n" );
+                    }
+
+                    fclose( fileWrite );
+                    printf(": : : : : : : : : : : : : : : : : : : : : \n");
+                    printf( "Lottery rows successfully saved to file \n" );
+                    printf(": : : : : : : : : : : : : : : : : : : : : \n");
+                    temp = 2;
+                }
+                else
+                {
+                    printf(": : : : : : : : : : : : : \n");
+                    printf( "Use menu choice 1 first \n" );
+                    printf(": : : : : : : : : : : : : \n");
+                    printf( "\n" );
+                }
 
                 break;
 
             case 3:
 
-                break;
+                fileRead = fopen( filename, "r" );
 
+                while( getline( &line, &len, fileRead ) != -1 )
+                {
+                    printf("%s", line);
+                }
+
+                fclose( fileRead );
+
+                printf( "\n" );
+                printf(": : : : : : : : : : : : : : : : : : : : : \n");
+                printf( "Lottery rows successfully read from file \n" );
+                printf(": : : : : : : : : : : : : : : : : : : : : \n");
+
+                temp = 3;
+
+                break;
 
             default:
                 break;
