fd9991a3a74734053974f94220514ce5e62920d2
Felix Wetell <felix.wetell@gmail.com>
2018-12-12 12:43:25 +0100

    Commented code for the teacher

--- C/Labs/lottery/main.c
+++ C/Labs/lottery/main.c
@@ -1,5 +1,6 @@
 #include "prefixes.h"
 
+// Searches for duplicates in array, by passing array and the number to the function
 int duplicate( int array[maxColumns], int number )
 {
     int answer = 0, i;
@@ -14,6 +15,7 @@ int duplicate( int array[maxColumns], int number )
     return answer;
 }
 
+// Randomising numbers with value 1-35 and puts the in an array passed as an argument
 int * randomise ( int array[maxColumns] )
 {
     int random;
@@ -33,20 +35,25 @@ int * randomise ( int array[maxColumns] )
 
 int main()
 {
+    // Initializing random number generator
     srand(time(NULL));
-    int input, rowInput, lotteryRows[maxRows][maxColumns], lotteryRow[maxColumns], temp = 0, rowTemp;
 
+    // Initializing variables
+    int input, rowInput, temp = 0;
+    int lotteryRow[maxColumns], lotteryRows[maxRows][maxColumns];
+
+    // Initializing file
     char* filename = "/Users/felix/Documents/Github/university/C/Labs/lottery/lottery.txt";
-
     FILE * fileWrite;
     FILE * fileRead;
 
+    // Variables for handling file output
     char * line = NULL;
     size_t len = 0;
 
     do
     {
-        // Menu
+        // Menu choices with input
         printf(": : : : : : : : : : : : \n");
         printf(": : : : M E N U : : : : \n");
         printf("1. Randomise lottery row\n");
@@ -60,23 +67,31 @@ int main()
 
         switch ( input )
         {
+            // Closes the program
             case 0:
+
                 printf( "Exiting program" );
                 break;
-
+            // Creates a selection of arrays containing random number
             case 1:
 
+                // Gets number of rows user want to have, max 10
                 printf( "Max rows allowed: 10\n" );
                 printf( "Enter number of rows: " );
                 scanf( "%i", &rowInput );
                 printf( "\n" );
 
+                // Checks if rows are more than 0 and less than 10
                 if( rowInput < 10 && rowInput > 1 )
                 {
+                    // Adds array with random numbers to 2D-array (lotteryRows)
                     for( int i = 0; i < rowInput; ++i )
                     {
+                        // Sends in lotteryRow to randomise function
                         randomise( lotteryRow );
 
+                        // Adds values from lotteryRow to lotteryRows at the correct position
+                        // and prints the to the console
                         for( int j = 0; j < maxColumns; ++j )
                         {
                             lotteryRows[ i ][ j ] = lotteryRow[j];
@@ -91,7 +106,6 @@ int main()
                     printf(": : : : : : : : : : : : : : : : : : : \n");
 
                     temp = 1;
-                    rowTemp = rowInput;
                 }
                 else if( rowInput > 10 )
                 {
@@ -106,19 +120,26 @@ int main()
 
             case 2:
 
-                if( temp >= 1 )
+                // Checks if user have randomised numbers
+                if( temp == 1 )
                 {
+                    // Opens file for writing
                     fileWrite = fopen( filename, "w" );
 
-                    for( int i = 0; i < rowTemp; ++i )
+                    // Prints values from x and y positions in lotteryRows to file
+                    // Gets the previous choosen number of rows
+                    for( int i = 0; i < rowInput; ++i )
                     {
+                        // maxColumns defined in "prefixes.h"
                         for( int j = 0; j < maxColumns; ++j )
                         {
                             fprintf( fileWrite, "%i ", lotteryRows[i][j] );
                         }
+                        // Adds line break for easier reading and better formatting
                         fprintf( fileWrite, "\n" );
                     }
 
+                    // Closes file
                     fclose( fileWrite );
                     printf(": : : : : : : : : : : : : : : : : : : : : \n");
                     printf( "Lottery rows successfully saved to file \n" );
@@ -137,21 +158,22 @@ int main()
 
             case 3:
 
+                // Opens file for reading
                 fileRead = fopen( filename, "r" );
 
+                // Looks on the file and prints each row (line) until file ends
                 while( getline( &line, &len, fileRead ) != -1 )
                 {
                     printf("%s", line);
                 }
 
+                // Closes file
                 fclose( fileRead );
 
                 printf( "\n" );
                 printf(": : : : : : : : : : : : : : : : : : : : : \n");
                 printf( "Lottery rows successfully read from file \n" );
                 printf(": : : : : : : : : : : : : : : : : : : : : \n");
-
-                temp = 3;
 
                 break;
 
@@ -160,6 +182,7 @@ int main()
         }
         printf( "\n" );
     }
+    // If input is 0 the do/while loop ends and the program exits
     while( input != 0 );
 
     return 0;
--- C/Labs/lottery/prefixes.h
+++ C/Labs/lottery/prefixes.h
@@ -4,8 +4,10 @@
 #include <string.h>
 #include <time.h>
 
+// Max and min value for lottery number values
 const int max = 34;
 const int min = 1;
 
+// Prefixed values for 2D array
 const int maxRows = 10;
 const int maxColumns = 7;
