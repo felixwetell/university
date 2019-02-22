c428a3a7273f385c74d7ab4b550bb4dbaae7f630
Felix Wetell <felix.wetell@gmail.com>
2019-02-22 15:02:49 +0100

    Code

--- Web Development/individual/js/game.js
+++ Web Development/individual/js/game.js
@@ -1,4 +1,4 @@
-$(document).bind('touchmove', function(e) {
+$(document).on('touchmove', function(e) {
 	e.preventDefault();
 });
 
@@ -28,8 +28,8 @@ $( function() {
     	controlArrows: true,
     	verticalCentered: true,
     	sectionsColor : ['#fff', '#f9ca24', '#f0932b', '#eb4d4b', '#6ab04c', '#be2edd', '#fff'],
-    	paddingTop: '3em',
-    	paddingBottom: '10px',
+    	paddingTop: '1em',
+    	paddingBottom: '1em',
     	fixedElements: '#header, .footer',
     	responsiveWidth: 0,
     	responsiveHeight: 0,
@@ -53,3 +53,64 @@ $( function() {
     	onSlideLeave: function(section, origin, destination, direction){}
     });
 });
+
+// $( 'form input' ).on( 'click', function( e )
+// {
+//
+// 	let input = $(this);
+// 	let label = input.parent();
+// 	console.log(label);
+// 	label.addClass( 'active' );
+// } );
+
+ $("form section input").click(function ()
+{
+	let input = $(this);
+	let group = input.attr( 'name' );
+
+	var i = 0;
+	$( 'input[name=' + group + ']' ).each( function()
+	{
+		i++;
+		switch( $(this).is( ':checked' ) )
+		{
+			case true:
+				$(this).parent().addClass( 'active' );
+				console.log( i + ' ' + $(this).is( ':checked' ) );
+				break;
+
+			case false:
+				console.log( i + ' ' + $(this).is( ':checked' ) );
+				$(this).parent().removeClass( 'active' );
+
+				break;
+			default:
+				console.log( 'Something went wrong' );
+				break;
+		}
+	});
+} );
+
+// $("form section input").click(function () {
+//
+//
+// 	let input = $(this);
+// 	console.log( input.is( ':checked' ) );
+// 	let group = input.attr( 'name' );
+// 	let label = input.parent();
+//
+// 	$( 'input[name=' + group + ']' ).each( function()
+// 	{
+// 		if( $(this).is( ':checked' ) )
+// 		{
+// 			console.log($(this));
+// 			label.addClass( 'active' );
+// 		}
+// 		else
+// 		{
+// 			// console.log('remove');
+// 			label.removeClass( 'active' );
+//
+// 		}
+// 	} );
+// });
